ae719925ae56de46f3b570636a30117f5215fbc8
Dmitrii Korshunov <dimakorsh.work@gmail.com>
2021-11-27 18:23:43 +0300

    With auth

---
+++ climate-cs/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
+
+
+
+function App() {
+  return (
+    <div>
+    <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
+
